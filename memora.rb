80a295fedfb4de35405843612489c8c64856b57d
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-01-18 00:03:12 -0800

    memora: 1.4.1

--- memora.rb
+++ memora.rb
@@ -1,8 +1,8 @@
 class Memora < Formula
   desc "Another spaced repetition flashcard app"
   homepage "https://github.com/jpalardy/memora"
-  url "https://github.com/jpalardy/memora/archive/1.4.0.tar.gz"
-  sha256 "9138e5af90ec9004932308cb29031b51a396238b3eec82d7affdf653ec8035f4"
+  url "https://github.com/jpalardy/memora/archive/1.4.1.tar.gz"
+  sha256 "87bbe0e4dd554ca9cac03040a710c6a1e17d845631b313f03a28474a3d9f6f91"
 
   depends_on "go" => :build
 
